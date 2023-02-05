c560075ca5b807de3d10241b53459c059e3b3f54
Millie Jackson <millie.jackson1@gmail.com>
2023-02-05 11:29:41 +0000

    Completed the functions and function calls unit

---
+++ 0 Essentials/Python Essentials/2 Functions And Function Calls/Practical.py
@@ -0,0 +1,10 @@
+print("This print statement was created in Python")
+
+my_string = "Hello World"
+print(len(my_string))
+my_length = len(my_string)
+print(my_length)
+
+my_number = 5
+print(len(my_number))
+my_length = len(my_number)
