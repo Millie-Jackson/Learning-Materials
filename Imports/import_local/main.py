eb2e7f92ac68d6adfc30b422220df41668b4b57a
Millie-Jackson <millie.jackson1@gmail.com>
2023-04-04 11:40:37 +0100

    Completed Imports Unit

---
+++ Imports/import_local/main.py
@@ -0,0 +1,14 @@
+from module_1 import function_1 
+from module_1 import function_2 
+from module_2 import function_1 
+from package_1.module_1 import function_package
+from module_2 import x
+
+#function_1()
+#function_2()
+
+#function_package()
+
+print(x)
+x = "Hello, I am in main.py"
+print(x)
