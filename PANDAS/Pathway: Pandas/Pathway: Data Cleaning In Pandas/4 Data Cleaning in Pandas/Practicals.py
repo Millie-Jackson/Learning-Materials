3b434f0ffb2e9820d7709a030651fbe2318db8e7
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2023-05-18 18:10:16 +0100

    Starting An AiCore MLOps Class

--- PANDAS/Pathway: Pandas/Pathway: Data Cleaning In Pandas/4 Data Cleaning in Pandas/Practicals.py
+++ PANDAS/Pathway: Pandas/Pathway: Data Cleaning In Pandas/4 Data Cleaning in Pandas/Practicals.py
@@ -19,21 +19,53 @@ flights_df['AIRLINECODE'] = flights_df['AIRLINECODE'].astype('category')
 #print(flights_df['AIRLINECODE'])
 
 duplicates = flights_df[["ORIGAIRPORTNAME", "DESTAIRPORTNAME", "AIRLINECODE", "FLIGHTDATE", "CRSDEPTIME", "DEPTIME", "ARRTIME"]].duplicated(keep=False)
+flights_df[duplicates]
 #print(flights_df[duplicates])
 #sort = duplicates.sort_values(ascending=False)
 #print(sort)
 
 summaries = {"CRSARRTIME": "mean", "ARRTIME": "mean", "ARRDELAY": "mean", "CRSELAPSEDTIME": "mean", "ACTUALELAPSEDTIME": "mean"}
+
 grouped_duplicates = flights_df[duplicates].groupby(["FLIGHTDATE", "AIRLINECODE", "ORIGAIRPORTNAME", "DESTAIRPORTNAME"], observed=True)
 grouped_duplicates_min_transactionid = grouped_duplicates["TRANSACTIONID"].min().reset_index()
+
 f_df_duplicates = pd.merge(
     grouped_duplicates_min_transactionid,
     grouped_duplicates.agg(summaries).reset_index(),
     how="inner").sort_values("TRANSACTIONID")
 
-remove_nan = f_df_duplicates.dropna().reset_index(drop=True)
+f_df_duplicates
+
+f_df_duplicates = f_df_duplicates.dropna().reset_index(drop=True)
 flights_df['TRANSACTIONID'] = flights_df['TRANSACTIONID'].astype('int64')
 #print(f_df_duplicates)
-print(remove_nan)
+#print(remove_nan)
+
+
+
+
+# Set index to a common column
+flights_df = flights_df.set_index("TRANSACTIONID")
+f_df_duplicates = f_df_duplicates.set_index("TRANSACTIONID")
+
+# Update old df with new data
+flights_df.update(f_df_duplicates)
+
+# Reset to original index
+flights_df.reset_index(inplace=True)
+
+
+# Find all unique items
+flights_df.at[0, "ORIGINSTATENAME"] = "FAKESTATE"
+states = flights_df["ORIGINSTATENAME"].unique()
+
+# Change a specific item
+#.iat[1, 0] = "FAKESTATE"
+
+
+print(type(flights_df))
+print(states)
+#print(states.iat[1, 1])
+#print(flights_df.iat[1, 1])
 
 print("hello world")
