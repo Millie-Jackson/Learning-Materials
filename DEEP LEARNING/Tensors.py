47e66e51a4d7d17adbcd9098505562864a2c9e41
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2023-06-30 12:09:28 +0100

    Tensor Indexing

--- DEEP LEARNING/Tensors.py
+++ DEEP LEARNING/Tensors.py
@@ -174,7 +174,36 @@ random_uniform_tensor = tf.random.uniform(
 #print(random_uniform_tensor)
 
 tf.random.set_seed(5)
-print(tf.random.uniform(shape=[3], maxval=5, dtype=tf.int32, seed=10))
-print(tf.random.uniform(shape=[3], maxval=5, dtype=tf.int32, seed=10))
-print(tf.random.uniform(shape=[3], maxval=5, dtype=tf.int32, seed=10))
-print(tf.random.uniform(shape=[3], maxval=5, dtype=tf.int32, seed=10))
+#print(tf.random.uniform(shape=[3], maxval=5, dtype=tf.int32, seed=10))
+#print(tf.random.uniform(shape=[3], maxval=5, dtype=tf.int32, seed=10))
+#print(tf.random.uniform(shape=[3], maxval=5, dtype=tf.int32, seed=10))
+#print(tf.random.uniform(shape=[3], maxval=5, dtype=tf.int32, seed=10))
+
+
+
+# INDEXING
+
+tensor_indexed = tf.constant([3, 6, 2, 4, 6, 66, 7])
+print(tensor_indexed)
+print(tensor_indexed[0:4]) # Same
+print(tensor_indexed[:4]) # Same
+print(tensor_indexed[0:]) # Assumes last element
+print(tensor_indexed[0:-1]) # Last element
+print(tensor_indexed[1:6:2]) # Go up in twos / Skip and element
+
+print(tensor_two_d[0:3, 0:2]) # First 2 columns of the first 3 rows
+print(tensor_two_d[0:3, :]) # All columns of the first 3 rows
+print(tensor_two_d[2, :])  # Second row
+print(tensor_two_d[2, :1])  # Second and third column of the second row
+print(tensor_two_d[:, 0])  # First element of each row
+print(tensor_two_d[1:3, 0])  # First element of first and second row
+print(tensor_two_d[..., 0]) # All the rows, first column
+
+print(tensor_three_d[4, 2, 3]) # Matches the shape
+print(tensor_three_d[0, :, :]) # First indexs, all rows, all columns
+print(tensor_three_d[0, 0, :]) # First indexs, first row, all columns
+print(tensor_three_d[0, :, -1]) # First indexs, all rows, last column
+print(tensor_three_d[0, :, 2]) # Same as above
+print(tensor_three_d[0:2, :, 2]) # First 2 indexs, all rows, last columns
+print(tensor_three_d[0:2, ..., 2]) # Same as above
+print(tensor_three_d[..., :, 2]) # All indexs, all rows, last columns
