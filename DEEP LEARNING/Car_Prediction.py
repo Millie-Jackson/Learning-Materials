fa1435bc4606cf6e5f12d5fcc418ebe91f7ab3ce
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2023-07-08 14:09:15 +0100

    Car Prediction 0.2

--- DEEP LEARNING/Car_Prediction.py
+++ DEEP LEARNING/Car_Prediction.py
@@ -2,12 +2,11 @@ import tensorflow as tf # For modeling
 import pandas as pd     # For data processing
 import seaborn as sns   # For visualization
 
+#  Load data set
 data = pd.read_csv("train.csv")
 
-sns.pairplot(data[["v.id", 
-                   "on road old", 
-                   "on road now", 
-                   "years", 
+# Visualise initial data set
+sns.pairplot(data[["years", 
                    "km", 
                    "rating", 
                    "condition", 
@@ -16,3 +15,25 @@ sns.pairplot(data[["v.id",
                    "hp", 
                    "torque", 
                    "current price"]], diag_kind="kde")
+
+## DATA PREPERATION  ##
+
+# CREATE TENSOR
+tensor_data = tf.constant(data)
+#print(tensor_data.shape)
+
+# TEST TENSOR
+tensor_data = tf.cast(tensor_data, tf.float32)
+#print(tensor_data)
+
+# SHUFFLE DATA SET
+tensor_data = tf.random.shuffle(tensor_data)
+
+# SET THE IMPUT VALUES
+x = tensor_data[:, 3:-1] # Skip the columns we dont want
+print(x.shape)
+
+# SET THE OUTPUT VALUES
+y = tensor_data[:, :-1] # Skip the columns we dont want
+y = tf.expand_dims(y, axis=-1) # Expand dimensions
+print(y.shape)
