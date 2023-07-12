5dd2d38c95a20772ee674649db1e1ba988c51c36
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2023-07-12 11:28:59 +0100

    Lists: Practical: Appending and Extending

---
+++ 26 Lists/Appending and Extending.py
@@ -0,0 +1,11 @@
+
+my_list_1 = [1, 2, 3, 4]
+my_list_2 = [5, 6, 7, 8]
+
+#my_list_1.append(my_list_2)
+print(my_list_1)
+print(my_list_2)
+
+my_list_1.extend(my_list_2)
+print(my_list_1)
+print(my_list_2)
